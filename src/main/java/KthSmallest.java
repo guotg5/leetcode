d2b5cb2fc17f8f1085cb496042fdf5117383e584
guotg5 <guotg5@yonyou.com>
2020-07-08 11:31:29 +0800

    二分法第k个数

--- src/main/java/KthSmallest.java
+++ src/main/java/KthSmallest.java
@@ -6,43 +6,40 @@ class KthSmallest {
 //    二分法   有序二维矩阵  如果确定一个数k 很容易得出比k小的数有多少个（从左下角遍历 直到横坐标超出）
 //    找出不小于k的数量  并记录k出现的次数
 //    如果不满足  二分法继续找数
-    //问题1 二分法的数不一定存在于数组
+    //问题1 必然存在多个数字 使得小于等于它本身的数量等于k  找到最小的
     public int kthSmallest(int[][] matrix, int k) {
-        int max = matrix[matrix.length-1][matrix.length-1];
-        int min = matrix[0][0];
-        return find(matrix, max, min, k);
+        int left = matrix[0][0];
+        int right = matrix[matrix.length-1][matrix.length-1];
+        int mid = left + (right - left)/2;
+
+        while(left < right){
+            if(check(matrix, mid, k)){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+            mid = left + (right - left)/2;
+        }
+        return left;
     }
 
-    public int find(int[][] arr,int max, int min, int k){
-        int mid = (max + min)/2;
-        int num = 0;
-        int x=arr.length-1;
-        int y=0;
-        while(x>=0&&y<arr.length){
-            if(arr[x][y]<=mid){
-                y++;
-                if(y==arr.length){
-                    num += y;
-                    x--;
-                    y--;
-                }
-            }else{
-                num += y;
-                x--;
-            }
-        }
-        if(num > k){
-            return find(arr,mid,min,k);
-        }else if (num < k){
-            return find(arr,max,mid,k);
-        }else{
-            return mid;
-        }
+    public boolean check(int[][] matrix, int mid, int k){
+       int x = matrix.length - 1;
+       int y = 0;
+       int num = 0;
+       while(x>=0&&y<matrix.length){
+           if(matrix[x][y] > mid){
+               x--;
+           }else{
+               num += (x + 1);
+               y++;
+           }
+       }
+       return num >= k;
     }
 
     public static void main(String[] args) {
-        new KthSmallest().kthSmallest(new int[][]{{1,2,3},
-                {4,5,6},{7,8,17}},1);
+        new KthSmallest().kthSmallest(new int[][]{{-5}},1);
     }
 
 }
