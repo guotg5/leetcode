22e70b8d8caed3f0fc4944059716f8c2bc3988fc
guotg5 <guotg5@yonyou.com>
2020-08-07 17:52:29 +0800

    fizzbuzz
    Change-Id: I6f9ab79815efc5e3be702f5e6b411b1725ca0131
    Signed-off-by: guotg5 <guotg5@yonyou.com>

--- src/多线程/FizzBuzz.java
+++ src/多线程/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.function.IntConsumer;
 
 /** 
  * @ClassName FizzBuzz 
- * @Description 顺序打印 
+ * @Description 顺序打印 1195
  * @author guotg
  * @date 2020-8-6 10:21:51 
  *  
@@ -21,57 +21,60 @@ class FizzBuzz {
 	Semaphore fizz = new Semaphore(0);
 	Semaphore buzz = new Semaphore(0);
 	Semaphore fizzbuzz = new Semaphore(0);
+	Semaphore common = new Semaphore(1);
 	// printFizz.run() outputs "fizz".
 	public void fizz(Runnable printFizz) throws InterruptedException {
-		fizz.acquire();
-		printFizz.run();
-		synchronized (this) {
-			this.notifyAll();
+		for(int i=1;i<=n;i++) {
+			if(i%3==0&&i%5!=0) {
+				fizz.acquire();
+				printFizz.run();
+				common.release();
+			}
 		}
-
 	}
 
 	// printBuzz.run() outputs "buzz".
 	public void buzz(Runnable printBuzz) throws InterruptedException {
-		buzz.acquire();
-		printBuzz.run();
-		synchronized (this) {
-			this.notifyAll();
+		for(int i=1;i<=n;i++) {
+			if(i%5==0&&i%3!=0) {
+				buzz.acquire();
+				printBuzz.run();
+				common.release();
+			}
 		}
 	}
 
 	// printFizzBuzz.run() outputs "fizzbuzz".
 	public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-		fizzbuzz.acquire();
-		printFizzBuzz.run();
-		synchronized (this) {
-			this.notifyAll();
+		for(int i=1;i<=n;i++) {
+			if(i%5==0&&i%3==0) {
+				fizzbuzz.acquire();
+				printFizzBuzz.run();
+				common.release();
+			}
 		}
 	}
 
 	// printNumber.accept(x) outputs "x", where x is an integer.
 	public void number(IntConsumer printNumber) throws InterruptedException {
 		while(count <= n) {
-			synchronized (this) {
+				common.acquire();
 				if(count%3==0&&count%5==0) {
 					fizzbuzz.release();
-					this.wait();
 				}else if(count%3==0) {
 					fizz.release();
-					this.wait();
 				}else if(count%5==0) {
 					buzz.release();
-					this.wait();
 				}else {
 					printNumber.accept(count);
+					common.release();
 				}
 				count++;
-			}
 		}
 	}
 
 	public static void main(String[] args) {
-		FizzBuzz fb = new FizzBuzz(10);
+		FizzBuzz fb = new FizzBuzz(15);
 		new Thread(() -> {
 			try {
 				fb.fizz(()->System.out.print(" fizz"));
