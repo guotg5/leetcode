04e9fa2dfb6e76c45c22f593bb7c982765666d02
guotiangang <guotiangang@qiyi.com>
2022-08-24 15:48:29 +0800

    链表相关

--- src/linkedList/AddTwoNumbers.java
+++ src/linkedList/AddTwoNumbers.java
@@ -175,16 +175,44 @@ public class AddTwoNumbers {
 	}
 
 	public static void main(String[] args) {
-		reorderList(ListNode.convert(new int[]{1,2,3,4,5}));
+		reverseList(ListNode.convert(new int[]{1,2,3,4,5}));
 	}
+
 	ListNode root = null;
 
-	public ListNode reverseList(ListNode head) {
-		if(head.next == null){
+	public static ListNode reverseList(ListNode head) {
+		if(head == null || head.next == null){
 			return head;
 		}
 
-		return reverseList(head);
+		ListNode result = reverseList(head.next);
+		head.next.next = head;
+		head.next = null;
+		return result;
+	}
+
+	public static ListNode mergeListNode(ListNode l1, ListNode l2) {
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+
+
+		ListNode resultHead = new ListNode(0);
+		ListNode result = resultHead;
+		while(true) {
+			if(l1 == null || l2 == null) {
+				result.next = l1 == null? l2 : l1;
+				break;
+			}
+			if(l1.val < l2.val) {
+				result.next = l1;
+				l1 = l1.next;
+			} else {
+				result.next = l2;
+				l2 = l2.next;
+			}
+			result = result.next;
+		}
+		return resultHead.next;
 	}
 
 	public static class ListNode {
